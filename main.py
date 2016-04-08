44e0f6a858ea626b8e315a2f9d5274f3eb71a7f8
Nihal Shriwastawa <nihal.sh.1995@gmail.com>
2016-04-08 16:33:19 +0530

    feed entry parsing added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import datetime
 import feedparser
 import json
 import sys
+from time import mktime
+from datetime import datetime
 
 from pymongo import MongoClient
 
@@ -9,7 +11,7 @@ feedJson = 'feed.json'
 
 def loadLinks(fileName):
         assert type(fileName) is str, "Invalid Argument: str expected!"
-        print fileName
+        #print fileName
         try:
                 with open(fileName) as fid:
                         feed = json.loads(fid.read())
@@ -18,6 +20,34 @@ def loadLinks(fileName):
                 sys.exit()
         else:
                 return feed
+
+def fetchFeeds(feed):
+        assert type(feed) is dict, "Invalid Argument: dict expected!"
+        for source in feed:
+                for news_type in feed[source]:
+                        for category in feed[source][news_type]:
+                                url = feed[source][news_type][category]
+
+                                doc = {}
+                                doc['source'] = source
+                                doc['news_type'] = news_type
+                                doc['category'] = category
+                                
+                                d = feedparser.parse(url)
+                                doc['feed_title'] = d.feed.title
+                                doc['feed_link_official'] = d.feed.link
+                                doc['feed_link_used'] = url
+                                doc['feed_published'] = datetime.fromtimestamp(mktime(d.feed.published_parsed))
+
+                                for entry in d.entries:
+                                        doc['title'] = entry.title
+                                        doc['id'] = entry.id
+                                        doc['link'] = entry.link
+                                        doc['published'] = datetime.fromtimestamp(mktime(entry.published_parsed))
+                                        doc['description'] = entry.description
+                                """add database functionality here"""
+                                print doc
+                                break
 
 client = MongoClient("192.168.101.5", 27017)
 db = client['news_feed']
@@ -30,4 +60,5 @@ DB --> master --> {'source': , 'feed_title':, 'feed_link_official': , 'feed_link
 
 
 
-loadLinks(feedJson)
+feed = loadLinks(feedJson)
+fetchFeeds(feed)
